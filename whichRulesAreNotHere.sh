aa2015a29487b0eb74a26b8cb336ab5d49975b32
Leni Aniva <v@leni.sh>
2023-05-27 00:01:20 -0700

    Add rewrite rule filter script

---
+++ whichRulesAreNotHere.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# which bv rewrite rules don't show up in regression tests?
+
+REWRITES=../cvc5/src/theory/bv/rewrites
+MKDSL_OUT=./rules/mkdsl-output.txt
+ALL=./rules/all.txt
+POSITIVE=./rules/positive.txt
+NEGATIVE=./rules/negative.txt
+
+python3 proof-rules.py rules -f $REWRITES > $ALL
+grep $MKDSL_OUT -e 'bv-' | sed -n 's/^.* \(.*\):.*$/\1/p' > $POSITIVE
+grep -F -x -v -f $POSITIVE $ALL > $NEGATIVE
