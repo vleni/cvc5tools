83c5a980b9af7051edc65c67b84ee129fda08b50
Leni Aniva <v@leni.sh>
2023-06-04 23:44:45 -0700

    Adapt the which rule script to linux

--- whichRulesAreNotHere.sh
+++ whichRulesAreNotHere.sh
@@ -9,5 +9,6 @@ POSITIVE=./rules/positive.txt
 NEGATIVE=./rules/negative.txt
 
 python3 proof-rules.py rules -f $REWRITES > $ALL
-grep $MKDSL_OUT -e 'bv-' | sed -n 's/^.* \(.*\):.*$/\1/p' > $POSITIVE
+#grep $MKDSL_OUT -e 'bv-' | sed -n 's/^.* \(.*\):.*$/\1/p' > $POSITIVE
+grep $MKDSL_OUT -e 'bv-' | sed 's/,.*//' > $POSITIVE
 grep -F -x -v -f $POSITIVE $ALL > $NEGATIVE
